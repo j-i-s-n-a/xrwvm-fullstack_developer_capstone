73305db684fbcaa529dba4b963a9915d2ed053bd
j-i-s-n-a <jisna293@gmail.com>
2026-04-26 09:46:18 -0400

    create car models

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
 from django.contrib import messages
 from datetime import datetime
+from .models import CarMake, CarModel
+from .populate import initiate
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -21,6 +23,17 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
@@ -98,3 +111,4 @@ def registration(request):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+
