d2789a473035c770fde8edd7634645993461654f
j-i-s-n-a <jisna293@gmail.com>
2026-04-30 13:04:55 -0400

    python file lint update

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -21,8 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =\
+SECRET_KEY = (
     'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,7 +34,7 @@ ALLOWED_HOSTS = [
     'your-long-domain-name-here.com'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://jisna293-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+'https://jisna293-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
     ]
 
 REST_FRAMEWORK = {
@@ -96,20 +97,24 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        {
+        'NAME': (
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator'
+    ),
+    },
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':('django.contrib.auth.password_validation.')
+        'MinimumLengthValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':('django.contrib.auth.password_validation.')
+        'CommonPasswordValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':('django.contrib.auth.password_validation.')
+        'NumericPasswordValidator',
     },
 ]
 
